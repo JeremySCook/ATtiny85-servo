4cbe40a57dc92d233eb3aa82716544348cd90d14
Jeremy S Cook <jscook55@gmail.com>
2019-12-16 17:17:41 -0500

    Update servo-delayMicroseconds.cpp

--- servo-delayMicroseconds.cpp
+++ servo-delayMicroseconds.cpp
@@ -12,14 +12,14 @@ for(int i=0; i<50; i++) {
 digitalWrite(0, HIGH);
 delayMicroseconds(600);
 digitalWrite(0, LOW);
-delay(10);
+delayMicroseconds(19400); //total period of 20ms = 20000us seems to be the correct number, have successfully used different value
 }
 delay(1000);
 for(int j=0; j<50; j++) {
 digitalWrite(0, HIGH);
 delayMicroseconds(2400);
 digitalWrite(0, LOW);
-delay(10);
+delayMicroseconds(17600);
 }
 delay(1000);
 }
