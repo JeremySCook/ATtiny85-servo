439e677063fff93c925b4b84ec8af53bef9896ad
Jeremy S Cook <jscook55@gmail.com>
2019-12-17 11:07:48 -0500

    Update servo-library-longerdelay.cpp

--- servo-library-longerdelay.cpp
+++ servo-library-longerdelay.cpp
@@ -12,7 +12,7 @@ void setup() {
 
 void loop() {
   for (pos = 0; pos <= 180; pos += 5) { // goes from 0 degrees to 180 degrees
-    // in steps of 1 degree
+    // in steps of 5 degrees
     myservo.write(pos);              // tell servo to go to position in variable 'pos'
     delay(30);                       // waits 15ms for the servo to reach the position
   }
